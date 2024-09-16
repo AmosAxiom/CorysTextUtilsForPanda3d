8cc782c417f4fe4b95980de7bcd270cc0d49b0d1
Dr Axiom <justificationmeanstheend@gmail.com>
2024-09-16 15:28:01 -0700

    Removed my debugging print statements

--- DirectGUISelectable.py
+++ DirectGUISelectable.py
@@ -262,9 +262,6 @@ class DirectEntrySelectable(DirectFrame):
 
 	#updates the selection box
 	def render_selection(self):
-		print('selection:', self.getTextSelection())
-		print(self.getSelectedText())
-
 
 		if self.__textSelectionStart == self.__textSelectionEnd:
 			self.__textHilightNodePath.hide()
@@ -752,5 +749,40 @@ class DirectEntrySelectable(DirectFrame):
 					   self.ur[2] + pad[1] + borderWidth[1]]
 		return self.bounds
 
-class DirectLabelSelectable:
-	pass
+class DirectLabelSelectable(DirectFrame):
+    """
+    DirectLabel(parent) - Create a DirectGuiWidget which has multiple
+    states.  User explicitly chooses a state to display
+    """
+
+    def __init__(self, parent = None, **kw):
+        # Inherits from DirectFrame
+        # A Direct Frame can have:
+        # - A background texture (pass in path to image, or Texture Card)
+        # - A midground geometry item (pass in geometry)
+        # - A foreground text Node (pass in text string or Onscreen Text)
+        # For a direct label:
+        # Each label has 1 or more states
+        # The same image/geom/text can be used for all states or each
+        # state can have a different text/geom/image
+        # State transitions happen under user control
+
+        optiondefs = (
+            # Define type of DirectGuiWidget
+            ('pgFunc',          PGItem,    None),
+            ('numStates',       1,         None),
+            ('state',           self.inactiveInitState, None),
+            ('activeState',     0,         self.setActiveState),
+        )
+        # Merge keyword options with default options
+        self.defineoptions(kw, optiondefs)
+
+        # Initialize superclasses
+        DirectFrame.__init__(self, parent)
+
+        # Call option initialization functions
+        self.initialiseoptions(DirectLabelSelectable)
+
+    def setActiveState(self):
+        """ setActiveState - change label to specifed state """
+        self.guiItem.setState(self['activeState'])
